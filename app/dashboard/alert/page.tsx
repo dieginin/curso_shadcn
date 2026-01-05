664909da5b067ce4334590be02f048875e218d32
dieginin <diegobalestra9@gmail.com>
2026-01-05 15:55:38 -0800

    Add Alert component with variants and integrate into the dashboard page

--- app/dashboard/alert/page.tsx
+++ app/dashboard/alert/page.tsx
@@ -1,7 +1,26 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { Check, Terminal, X } from "lucide-react"
+
 export default function Page() {
   return (
-    <div>
-      <h1>Alert Page</h1>
+    <div className='grid gap-3'>
+      <Alert>
+        <Terminal />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>You can add components and dependencies to your app using the cli.</AlertDescription>
+      </Alert>
+
+      <Alert variant='destructive'>
+        <X />
+        <AlertTitle>Destructive</AlertTitle>
+        <AlertDescription>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, quisquam.</AlertDescription>
+      </Alert>
+
+      <Alert variant='success'>
+        <Check />
+        <AlertTitle>Success</AlertTitle>
+        <AlertDescription>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, cumque?</AlertDescription>
+      </Alert>
     </div>
   )
 }
