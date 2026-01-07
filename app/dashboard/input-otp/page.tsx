90a7ae69ff6bfcfae518d00d3786791e80bc13a4
dieginin <diegobalestra9@gmail.com>
2026-01-06 21:29:58 -0800

    feat: add Input OTP component and integrate into the dashboard layout

---
+++ app/dashboard/input-otp/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot } from "@/components/ui/input-otp"
+
+import { useState } from "react"
+
+export default function Page() {
+  const [value, setValue] = useState("")
+
+  return (
+    <div className='flex flex-col justify-center items-center h-62.5'>
+      <InputOTP maxLength={6} value={value} onChange={setValue}>
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+        </InputOTPGroup>
+        <InputOTPSeparator />
+        <InputOTPGroup>
+          <InputOTPSlot index={2} />
+          <InputOTPSlot index={3} />
+        </InputOTPGroup>
+        <InputOTPSeparator />
+        <InputOTPGroup>
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+        </InputOTPGroup>
+      </InputOTP>
+
+      <div className='mt-5'>{value}</div>
+    </div>
+  )
+}
