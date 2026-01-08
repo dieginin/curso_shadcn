3a4fbd67db8916e1bda934212f60eb91d445a0ed
dieginin <diegobalestra9@gmail.com>
2026-01-08 01:44:05 -0800

    feat: implement theme toggle functionality with dropdown menu and create ThemeProvider component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css"
 import { Geist, Geist_Mono } from "next/font/google"
 
 import type { Metadata } from "next"
+import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/sonner"
 
 const geistSans = Geist({
@@ -26,10 +27,12 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
-        <Toaster />
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+          {children}
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   )
