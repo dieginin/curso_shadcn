f1394e72d8b98f48c3f19236e6a7dd8b76016cbe
dieginin <diegobalestra9@gmail.com>
2026-01-07 23:50:48 -0800

    feat: implement form handling with validation for username and email fields

--- app/dashboard/form/page.tsx
+++ app/dashboard/form/page.tsx
@@ -1,9 +1,69 @@
 "use client"
 
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+type FormData = z.infer<typeof formSchema>
+const formSchema = z.object({
+  username: z.string().min(2).max(20),
+  email: z.email(),
+})
+
 export default function Page() {
+  const form = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+    },
+  })
+
+  function onSubmit(values: FormData) {
+    console.log(values)
+  }
+
   return (
     <div>
-      <h1>Home Page</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='grid grid-cols-2 gap-4'>
+          <FormField
+            control={form.control}
+            name='username'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder='shadcn' {...field} />
+                </FormControl>
+                <FormDescription>This is your public display name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder='shadcn@example.com' {...field} />
+                </FormControl>
+                <FormDescription>Your contact information.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type='submit'>Submit</Button>
+        </form>
+      </Form>
     </div>
   )
 }
