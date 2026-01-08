a90d329dc7c84c7d0afd93827485b5fb26369373
dieginin <diegobalestra9@gmail.com>
2026-01-07 18:36:00 -0800

    feat: add DropdownMenu component and integrate actions in DataTable

--- app/dashboard/data-table/columns.tsx
+++ app/dashboard/data-table/columns.tsx
@@ -1,8 +1,20 @@
 "use client"
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
 import { ColumnDef } from "@tanstack/react-table"
+import { MoreHorizontal } from "lucide-react"
 import { Payment } from "@/data/payments.data"
+import { toast } from "sonner"
 
 export const columns: ColumnDef<Payment>[] = [
   {
@@ -44,5 +56,36 @@ export const columns: ColumnDef<Payment>[] = [
   {
     accessorKey: "email",
     header: "Email",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='ghost' className='w-8 h-8 p-0'>
+              <span className='sr-only'>Open menu</span>
+              <MoreHorizontal className='w-4 h-4' />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => {
+                navigator.clipboard.writeText(payment.id)
+                toast.success("Payment ID copied to clipboard", { position: "top-right", richColors: true })
+              }}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
   },
 ]
